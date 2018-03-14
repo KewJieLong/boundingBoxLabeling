99a1dd6f24417727a14661d99fefd3aa9c43980b
Kew Jie Long <kewjielong@gmail.com>
2018-03-14 18:15:40 +0800

    able configuration of extract_video_frame with args instead of hardcoded

--- util/extract_video_frame.py
+++ util/extract_video_frame.py
@@ -1,28 +1,41 @@
 import cv2
 import os
 import sys
+import argparse
 
 base_path = 'data'
-x_path = os.path.join(base_path, 'video')
-target_path = os.path.join(base_path, 'frame')
 count = 0
-for video in os.listdir(x_path):
-	print(os.path.join(x_path, video))
-	vidcap = cv2.VideoCapture(os.path.join(x_path, video))
 
-	success = True
-	while success:
-		try:
-			success, image = vidcap.read()
-			sys.stdout.write('\rsaving {}'.format(os.path.join(target_path, 'frame_{}.png'.format(count))))
-			sys.stdout.flush()
-			cv2.imwrite(os.path.join(target_path, 'frame_{}.png'.format(count)), image)
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='extract frame by frame from video')
+	parser.add_argument('-v', '--video_path', help='path to the video')
+	parser.add_argument('-t', '--target_path', help='path to the target')
 
-			if cv2.waitKey(10) == 27:
-				break
-			count += 1
-		except:
-			import traceback
-			print(traceback.print_exc())
+	args = parser.parse_args()
+	video_path = args.video_path
+	target_path = args.target_path
+
+	if os.path.exists(target_path) is False:
+		os.mkdir(target_path)
+
+
+	for video in os.listdir(video_path):
+		print(os.path.join(video_path, video))
+		vidcap = cv2.VideoCapture(os.path.join(video_path, video))
+
+		success = True
+		while success:
+			try:
+				success, image = vidcap.read()
+				sys.stdout.write('\rsaving {}'.format(os.path.join(target_path, 'frame_{}.png'.format(count))))
+				sys.stdout.flush()
+				cv2.imwrite(os.path.join(target_path, 'frame_{}.png'.format(count)), image)
+
+				if cv2.waitKey(10) == 27:
+					break
+				count += 1
+			except:
+				import traceback
+				print(traceback.print_exc())
 
 
